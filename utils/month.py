7ee53323831fb096667c006c3408c30e4d20d7d3
mbay-SAG <58465677+mbay-SAG@users.noreply.github.com>
2021-01-10 10:02:01 +0100

    Update month.py
    Bug that month was not returned correctly

--- utils/month.py
+++ utils/month.py
@@ -3,6 +3,6 @@ import sys
 def monthInt_to_string(month):
     try:
         monate = ("Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember")
-        return monate[month+1]
+        return monate[month-1]
     except:
         return "Unknown"
