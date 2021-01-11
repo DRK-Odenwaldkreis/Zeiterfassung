51a117eaa0b1493fbc564036633b933ad3e5c2b6
Murat Bayram (y509177) <Murat.Bayram@softwareag.com>
2021-01-11 11:09:35 +0100

    logging in file again

--- EinzelAbrechnungPDF/abrechnung_einzeln_pdf.py
+++ EinzelAbrechnungPDF/abrechnung_einzeln_pdf.py
@@ -11,7 +11,7 @@ import logging
 
 
 logFile = '../Logs/singleReportJob.log'
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(filename=logFile,level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('Single Report')
 logger.debug('Starting')
