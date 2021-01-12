d54a5516ea35e6986e804e3c5f39beb470858cbb
Marc-DA <web@marc-duchene.de>
2021-01-12 20:20:35 +0100

    merging

---
+++ web/server_settings.php
@@ -0,0 +1,19 @@
+<?php
+
+/* **************
+
+Zeiterfassung für das Impfzentrum DRK Odenwaldkreis
+Author: Marc S. Duchene
+January 2021
+
+** ************** */
+
+	/* Database connection information */
+    $gaSql_server['user']       = "webservice";
+    $gaSql_server['password']   = 'SSD+51/I#*JY';
+    $gaSql_server['db']         = "impfzentrum";
+    $gaSql_server['server']     = "localhost";
+    $gaSql_server['port']  = "3306";
+	
+
+?>
