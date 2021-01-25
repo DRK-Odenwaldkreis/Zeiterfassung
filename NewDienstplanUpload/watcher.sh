67d4bb070a5cab2943ea3a0ed1edd6fe742fc9c1
Murat Bayram (y509177) <Murat.Bayram@softwareag.com>
2021-01-25 18:12:25 +0100

    adapated watcher of new dienstplaene to correct time frame

--- NewDienstplanUpload/watcher.sh
+++ NewDienstplanUpload/watcher.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Starting check for new Dienstplaene with hysterese"
-if [[ -n $(find /home/webservice/Planung/* -amin +0 -amin -299) ]]
+if [[ -n $(find /home/webservice/Planung/* -amin +239 -amin -299) ]]
 then
 echo "Result is true"
-year=$(find /home/webservice/Planung/* -amin +0 -amin -299 | cut -d'/' -f5 | cut -d'_' -f2)
-week=$(find /home/webservice/Planung/* -amin +0 -amin -299 | cut -d'/' -f5 | cut -d'_' -f4 | cut -d'.' -f1)
+year=$(find /home/webservice/Planung/* -amin +239 -amin -299 | cut -d'/' -f5 | cut -d'_' -f2)
+week=$(find /home/webservice/Planung/* -amin +239 -amin -299 | cut -d'/' -f5 | cut -d'_' -f4 | cut -d'.' -f1)
 echo $year
 echo $week
 python3 ./job.py $week $year
