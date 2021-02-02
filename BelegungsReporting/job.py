0a657c495d537ce7250daf0615cd5c5817b7d476
Murat Bayram (y509177) <Murat.Bayram@softwareag.com>
2021-02-02 10:30:18 +0100

    Logger name for Belegungseporting

--- BelegungsReporting/job.py
+++ BelegungsReporting/job.py
@@ -8,7 +8,7 @@ from utils.database import Database
 logFile = '../../Logs/rotationJob.log'
 logging.basicConfig(filename=logFile,level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger('Reminder Planning')
+logger = logging.getLogger('Besetzung Reporting')
 logger.debug('Starting')
 
 if __name__ == "__main__":
