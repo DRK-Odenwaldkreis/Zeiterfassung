ea52b8897235dd31327aac4068490a5b8cd60317
root <root@familie-bayram.de>
2021-12-05 21:57:13 +0100

    adaption of planungds pdf

--- DienstplanungPDF/pdfcreator/pdf.py
+++ DienstplanungPDF/pdfcreator/pdf.py
@@ -101,35 +101,40 @@ class PDFgenerator:
 				logger.debug('Pagertrigger was achieved, creating new page.')
 				self.new_page()
 			for count, value in enumerate(self.list):
-				if count == 0:
-					logger.debug('Writing the frueh shift')
-					try:
-						self.pdf.cell(70, 5, value[line][:20], ln=0)
-					except:
-						self.pdf.cell(70, 5, '', ln=0)
-				elif count == 1:
-					logger.debug('Writing the spaet shift')
-					try:
-						self.pdf.cell(70, 5, value[line][:20], ln=0)
-					except:
-						self.pdf.cell(70, 5, '', ln=0)
-				elif count == 2:
-					logger.debug('Writing the variable shift')
-					self.pdf.set_font('GNU', '', 10)
-					try:
-						self.pdf.cell(70, 5, value[line][:35], ln=1)
-					except:
-						self.pdf.cell(70, 5, '', ln=1)
-				elif count == 3:
-					logger.debug('Writing the variable shift')
-					self.pdf.set_font('GNU', '', 10)
-					try:
-						self.pdf.cell(80, 5, value[line][:45], ln=1)
-					except:
-						self.pdf.cell(80, 5, '', ln=1)
-					finally:
-						logger.debug('Resetting font size')
-						self.pdf.set_font('GNU', '', 14)
+				try:
+					if count == 0:
+						logger.debug('Writing the frueh shift')
+						try:
+							self.pdf.cell(70, 5, value[line][:30], ln=0)
+						except:
+							self.pdf.cell(70, 5, '', ln=0)
+					elif count == 1:
+						logger.debug('Writing the middle shift')
+						try:
+							self.pdf.cell(70, 5, value[line][:30], ln=0)
+						except:
+							self.pdf.cell(70, 5, '', ln=0)
+					elif count == 2:
+						logger.debug('Writing the spaet shift')
+						try:
+							self.pdf.cell(70, 5, value[line][:30], ln=0)
+						except:
+							self.pdf.cell(70, 5, '', ln=0)
+					elif count == 3:
+						logger.debug('Writing the variable shift')
+						self.pdf.set_font('GNU', '', 10)
+						try:
+							self.pdf.cell(70, 5, value[line][:45], ln=1)
+						except:
+							self.pdf.cell(70, 5, '', ln=1)
+						finally:
+							logger.debug('Resetting font size')
+							self.pdf.set_font('GNU', '', 14)
+				except:
+					pass
+				finally:
+					logger.debug('Resetting font size')
+					self.pdf.set_font('GNU', '', 14)
 
 	def new_page(self):
 		self.pdf.add_font('GNU', '', FreeSans, uni=True)
@@ -161,6 +166,7 @@ class PDFgenerator:
 		self.leftColumn = []
 		self.centerColumn = []
 		self.rightColumn = []
+		self.alternateColumn = []
 		self.new_page()
 		for i in self.content:
 			logger.debug('Found the following entry: %s' % (str(i)))
@@ -172,19 +178,19 @@ class PDFgenerator:
 				self.pdf.set_font('GNU', '', 14)
 				if self.shift == 1:
 					self.leftColumn.append('%s, %s' % (self.currentNachname, self.currentVorname))
+				elif self.shift == 2:
+					self.rightColumn.append('%s, %s' % (self.currentNachname, self.currentVorname))
+				elif self.shift == 3:
+					self.alternateColumn.append('(%s) - %s, %s' % (i[4],self.currentNachname, self.currentVorname))
 				elif self.shift == 4:
 					self.centerColumn.append('%s, %s' % (self.currentNachname, self.currentVorname))
-				elif self.shift == 2:
-					self.centerColumn.append('%s, %s' % (self.currentNachname, self.currentVorname))
-				elif self.shift == 3:
-					self.rightColumn.append('(%s) - %s, %s' % (i[4],self.currentNachname, self.currentVorname))
 				else:
 					logger.warning('The given shift does not exist')
 			else:
 				# No planning entry is included for that day
 				pass
 		logger.debug('Finished with the content of day: %s'%(self.day))
-		self.list = [self.leftColumn, self.centerColumn, self.rightColumn]
+		self.list = [self.leftColumn, self.centerColumn, self.rightColumn, self.alternateColumn]
 		self.write_lines(self.list)
 
 
